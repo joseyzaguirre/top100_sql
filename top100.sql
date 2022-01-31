abff6c37f3850e110b9f656c7e2e939ab3cfa43c
José Eyzaguirre <josemanuel.eyzaguirre@gmail.com>
2022-01-31 18:53:53 -0300

    desafío hasta requerimiento 6

--- top100.sql
+++ top100.sql
@@ -32,3 +32,7 @@ select actor from reparto where pelicula_id=2;
 select pelicula.nombre_pelicula from pelicula join reparto on pelicula.id = reparto.pelicula_id where reparto.actor='Harrison Ford';
 
 --requerimiento 6
+
+select nombre_pelicula, anio from pelicula where anio >= 1990 and anio <= 1999 order by nombre_pelicula asc;
+
+-- requerimiento 7
